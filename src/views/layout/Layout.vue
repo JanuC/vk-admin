60dfce6333c4c56ff493881991b015f7cab2c9e5
janu <641578178@qq.com>
2024-06-21 23:00:26 +0800

    集成自动引入icon

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,7 +1,12 @@
 <template>
   <p class="text-2xl font-bold bg-red-900">Hello Tailwind!</p>
   <p>{{ test }}</p>
-  <el-input v-model="test" style="width: 240px" placeholder="Please input" />
+  <el-input v-model="test" style="width: 240px" placeholder="Please input">
+    <template #suffix>
+      <i-ep-search />
+    </template>
+  </el-input>
+  <i-ep-search />
 </template>
 
 <script lang="ts" setup>
