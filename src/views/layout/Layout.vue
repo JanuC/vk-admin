d1b9f4558db54b700c32dce4aca7ed926b919d34
JanuC <641578178@qq.com>
2024-06-21 17:46:28 +0800

    集成twailwind

---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,7 @@
+<template>
+  <p class="text-2xl font-bold bg-red-900">Hello Tailwind!</p>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
   plugins: [vue()],
 })
