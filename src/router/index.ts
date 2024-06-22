de7d0519f5ef0f4ce60bf2cb8a90f12f377e3315
janu <641578178@qq.com>
2024-06-22 09:21:13 +0800

    集成顶部进度条

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,20 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-
-import routes from './routes'
+import { createRouter, createWebHashHistory } from "vue-router"
+import { openProgress, closeProgress } from "@/utils/NProgress"
+import routes from "./routes"
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+})
+
+router.beforeEach(() => {
+  // 开启顶部进度条
+  openProgress()
+})
+
+router.afterEach(() => {
+  // 关闭顶部进度条
+  closeProgress()
 })
 
 export default router
